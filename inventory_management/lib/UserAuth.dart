ee96fff05139de315a4b7fcf1cd3a21f4ba1e9d8
Ajay0978 <88025421+Ajay0978@users.noreply.github.com>
2022-01-17 13:02:26 +0530

    Ui modified

--- inventory_management/lib/UserAuth.dart
+++ inventory_management/lib/UserAuth.dart
@@ -6,104 +6,92 @@ class UserAuth extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Navir",style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
-              backgroundColor: Colors.red
-            ),),
+    GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Form(
+          key: formkey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 18.0),
+                    child: Text(
+                      "Navir",
+                      style: TextStyle(
+                          fontSize: 100,
+                          color: Colors.white,
+                          backgroundColor: Colors.red),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 20, right: 10, left: 10),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      color: Colors.black12,
+                      padding: EdgeInsets.only(left: 12),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "required name";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter Your name",
+                          border: InputBorder.none,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                         Container(
+                  padding: EdgeInsets.only(top: 20, right: 10, left: 10),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      color: Colors.black12,
+                      padding: EdgeInsets.only(left: 12),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "required id number";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter Your ID number",
+                          border: InputBorder.none,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CupertinoButton(
+                      color: CupertinoColors.systemBlue,
+                      child: Text("Submit"),
+                      onPressed: () {
+                        if (formkey.currentState!.validate()) {
+                          (formkey.currentState!.save());
+                          print("Information saved successfully");
+                        }
+                      }),
+                )
+              ],
+            ),
           ),
         ),
-          Container(
-            padding:EdgeInsets.only(top:20,right:10,left:10),
-            child:Card(
-              shape:RoundedRectangleBorder(
-                borderRadius:BorderRadius.circular(10),
-              ),
-              child: Container(
-                color: Colors.black12,
-                padding:EdgeInsets.only(left:12),
-                child: TextFormField(
-                  decoration:InputDecoration(
-                    hintText:"Enter Your name",
-                    border:InputBorder.none,
-                    fillColor:Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            padding:EdgeInsets.only(top:20,right:10,left:10),
-            child:Card(
-              shape:RoundedRectangleBorder(
-                borderRadius:BorderRadius.circular(10),
-              ),
-              child: Container(
-                color: Colors.black12,
-                padding:EdgeInsets.only(left:12),
-                child: TextFormField(
-                  decoration:InputDecoration(
-                    hintText:"Enter Your Phone number",
-                    border:InputBorder.none,
-                    fillColor:Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            padding:EdgeInsets.only(top:20,right:10,left:10),
-            child:Card(
-              shape:RoundedRectangleBorder(
-                borderRadius:BorderRadius.circular(10),
-              ),
-              child: Container(
-                color: Colors.black12,
-                padding:EdgeInsets.only(left:12),
-                child: TextFormField(
-                  decoration:InputDecoration(
-                    hintText:"Enter Your Email",
-                    border:InputBorder.none,
-                    fillColor:Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            padding:EdgeInsets.only(top:20,right:10,left:10),
-            child:Card(
-              shape:RoundedRectangleBorder(
-                borderRadius:BorderRadius.circular(10),
-              ),
-              child: Container(
-                color: Colors.black12,
-                padding:EdgeInsets.only(left:12),
-                child: TextFormField(
-                  decoration:InputDecoration(
-                    hintText:"Enter Your ID number",
-                    border:InputBorder.none,
-                    fillColor:Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CupertinoButton(
-              color: CupertinoColors.systemBlue,
-                child: Text("Submit"),
-                onPressed: (){}
-                ),
-          )
-        ],
       ),
     );
   }
